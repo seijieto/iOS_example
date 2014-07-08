5929cdb5fd789f8316665146bcd610d0162050c2
Seiji Eto <seijieto@gmail.com>
2014-07-08 12:13:00 -0500

    first commit

---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Seiji Eto on 14/07/08.
+//  Copyright (c) 2014年 Cryptsonic, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "sampleAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([sampleAppDelegate class]));
+    }
+}
---
+++ ToDoList/sampleAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  sampleAppDelegate.h
+//  ToDoList
+//
+//  Created by Seiji Eto on 14/07/08.
+//  Copyright (c) 2014年 Cryptsonic, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sampleAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
